03d83f2fbd808118bd474a8ef9777dfb8309a4ed
mihai12 <mihai.popa@windowslive.com>
2016-08-27 22:35:49 +0000

    Added like/dislike functionality.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,45 +10,53 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
 Route::get('/', function () {
-  return view('welcome');
+    return view('welcome');
 })->name('home');
-
-Route::get('/dashboard', [
-  'uses' => 'PostController@getDashboard',
-  'as' => 'dashboard',
-  'middleware' => 'auth'
-]);
-
-Route::post('/signin', [
-  'uses' => 'UserController@postSignIn',
-  'as' => 'signin'
-]);
-
-Route::get('/logout', [
-  'uses' => 'UserController@getLogout',
-  'as' => 'logout'
-]);
-
 Route::post('/signup', [
-  'uses' => 'UserController@postSignUp',
-  'as' => 'signup'
+    'uses' => 'UserController@postSignUp',
+    'as' => 'signup'
 ]);
-
+Route::post('/signin', [
+    'uses' => 'UserController@postSignIn',
+    'as' => 'signin'
+]);
+Route::get('/logout', [
+    'uses' => 'UserController@getLogout',
+    'as' => 'logout'
+]);
+Route::get('/account', [
+    'uses' => 'UserController@getAccount',
+    'as' => 'account'
+]);
+Route::post('/upateaccount', [
+    'uses' => 'UserController@postSaveAccount',
+    'as' => 'account.save'
+]);
+Route::get('/userimage/{filename}', [
+    'uses' => 'UserController@getUserImage',
+    'as' => 'account.image'
+]);
+Route::get('/dashboard', [
+    'uses' => 'PostController@getDashboard',
+    'as' => 'dashboard',
+    'middleware' => 'auth'
+]);
 Route::post('/createpost', [
-  'uses' => 'PostController@postCreatePost',
-  'as' => 'post.create',
-  'middleware' => 'auth'
+    'uses' => 'PostController@postCreatePost',
+    'as' => 'post.create',
+    'middleware' => 'auth'
 ]);
-
-Route::post('/edit', [
-  'uses' => 'PostController@postEditPost',
-  'as' => 'edit'
-]);
-
 Route::get('/delete-post/{post_id}', [
-  'uses' => 'PostController@getDeletePost',
-  'as' => 'post.delete',
-  'middleware' => 'auth'
+    'uses' => 'PostController@getDeletePost',
+    'as' => 'post.delete',
+    'middleware' => 'auth'
+]);
+Route::post('/edit', [
+    'uses' => 'PostController@postEditPost',
+    'as' => 'edit'
+]);
+Route::post('/like', [
+    'uses' => 'PostController@postLikePost',
+    'as' => 'like'
 ]);
--- app/User.php
+++ app/User.php
@@ -10,4 +10,8 @@ class User extends Model implements Authenticatable {
     public function posts() {
       return $this->hasMany('App\Post');
     }
+
+    public function likes() {
+      return $this->hasMany('App\Like');
+    }
 }
